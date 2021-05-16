c86b7bb408411258637665ccb212aaf5f4978dae
youngUnPark <mduddns@naver.com>
2021-05-16 23:36:28 +0900

    Chore: 라우팅 라이브러리 추가

---
+++ front/src/components/Header.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="p-grid">
+    <div class="p-col-12 p-md-6 p-lg-3"><div class="box-row"><a href="/">HOME</a></div></div>
+    <div class="p-col-12 p-md-6 p-lg-3"><div class="box-row"><a href="study">STUDY</a></div></div>
+    <div class="p-col-12 p-md-6 p-lg-3"><div class="box-row"><a href="development">DEVELOPEMENT</a></div></div>
+    <div class="p-col-12 p-md-6 p-lg-3"><div class="box-row"><a href="admin">ADMIN</a></div></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Header"
+}
+</script>
+
+<style scoped>
+.box-row {
+  height: 13vw;
+  font-size: 2rem;
+  text-align: center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: 1px solid #ffffff;
+  border-radius: 20px;
+  box-shadow: 10px 5px 5px black;
+  background-color: #2196F3;
+}
+
+</style>
--- front/src/main.js
+++ front/src/main.js
@@ -1,15 +1,20 @@
-import {createApp} from 'vue';
-import App from './App.vue';
+import { createApp } from 'vue';
+import App from "@/App";
+import router from './router'
 import PrimeVue from 'primevue/config';
+import Button from "primevue/button";
+import InputText from "primevue/inputtext";
+//css
 import "primevue/resources/themes/saga-blue/theme.css"       //theme
 import "primevue/resources/primevue.min.css"                 //core css
 import "primeicons/primeicons.css"                           //icons
-import Button from "primevue/button";
-import InputText from "primevue/inputtext";
-const app = createApp(App);
+import 'primeflex/primeflex.css';                            //flex css
 
-app.use(PrimeVue);
+const app = createApp(App);
+console.log(1);
+app.use(router);
+app.use(PrimeVue, {ripple: true});
 app.component('Button',Button);
 app.component('InputText',InputText);
-app.mount('#app')
-
+console.log(10);
+app.mount('#app');
